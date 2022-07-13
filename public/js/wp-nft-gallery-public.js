f9ddc164db2cd7cc480d1ceecdd36b1bd490e20c
Rafa Soler <rafasoler10@gmail.com>
2022-07-13 20:34:21 +0200

    Connect gallery with Objkt API & setup Vue components

--- public/js/wp-nft-gallery-public.js
+++ public/js/wp-nft-gallery-public.js
@@ -1,32 +1,148 @@
-(function( $ ) {
-	'use strict';
+const { __, _x, _n, _nx, sprintf } = wp.i18n;
 
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
+/**
+ * Component for displaying the gallery filters.
+ */
+Vue.component('gallery-filters', {
+    template: `
+        <div class="nft-gallery-filters">
+            <h2>{{ __('My Filters', 'nft-gallery') }}</h2>
+        </div>
+    `,
+});
 
-})( jQuery );
+/**
+ * Component for displaying a list of gallery items (tokens).
+ */
+Vue.component('token-list', {
+    props: {
+        items: {
+            type: Array,
+            required: true,
+        },
+    },
+    template: `
+        <div class="nft-gallery-token-wrapper">
+            <p v-for="item in items">{{ item.token.name }}</p>
+        </div>
+    `,
+});
+
+/**
+ * Root component for the NFT gallery.
+ */
+const app = new Vue({
+    el: '#nft-gallery',
+
+    data: {
+        items: [],
+        isLoading: true,
+        ipfs_url: 'https://ipfs.io/ipfs/',
+    },
+
+    computed: {
+        siteUrl() {
+            return nftGallerySettings.site_url;
+        },
+        wpApiUrl() {
+            return nftGallerySettings.wp_api_url;
+        },
+        objktEndpoint() {
+            return nftGallerySettings.objkt_endpoint;
+        },
+        objktAlias() {
+            return nftGallerySettings.objkt_alias;
+        },
+    },
+
+    methods: {
+        /**
+         * Load the items from the specified marketplace API
+         *
+         * @param {string} [marketplace=Objkt] - The marketplace to load items from
+         *
+         */
+        async getItems(marketplace = 'Objkt') {
+            let items = [];
+            switch (marketplace) {
+                case 'Objkt':
+                    items = await this.getItemsFromObjkt();
+                    break;
+                default:
+                    console.warn(`Selected marketplace "${marketplace}" not supported`);
+                    break;
+            }
+            return items;
+        },
+
+        /**
+         * Load the items from the Objkt GraphQL API
+         *
+         */
+        async getItemsFromObjkt() {
+            console.debug('Objkt endpoint: ', this.objktEndpoint);
+            console.debug('Objkt alias: ', this.objktAlias);
+
+            const query = `query GetTokens($alias: String!) {
+                listing(
+                    where: {
+                        status: { _eq: "active" }, 
+                        token: { creators: { holder: { alias: { _eq: $alias } } } },
+                    },
+                    order_by: { token: { timestamp: desc } }
+                ) {
+                    id
+                    amount_left
+                    currency_id
+                    price
+                    fa_contract
+                    token {
+                        token_id
+                        name
+                        description
+                        supply
+                        timestamp
+                        thumbnail_uri
+                        artifact_uri
+                        mime
+                        tags {
+                            tag { name }
+                        }
+                    }
+                }
+            }`;
+
+            const response = await fetch(this.objktEndpoint, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                },
+                body: JSON.stringify({
+                    query,
+                    variables: { alias: this.objktAlias },
+                }),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                console.debug('Objkt data: ', data);
+                return data.data.listing;
+            } else {
+                console.error('Error fetching items from Objkt API');
+                return [];
+            }
+        },
+    },
+
+    async created() {
+        this.items = await this.getItems();
+    },
+
+    template: `
+		<div class="nft-gallery">
+            <gallery-filters></gallery-filters>
+            <token-list :items="items"></token-list>
+		</div>
+    `,
+});
